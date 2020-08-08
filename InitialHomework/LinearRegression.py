55fa6b27c49ec360643ffa3be8464aa0ee901433
CCranney <mellowcello13@gmail.com>
2020-08-08 13:42:29 -0500

    Package upload

--- InitialHomework/LinearRegression.py
+++ InitialHomework/LinearRegression.py
@@ -1,2 +1,15 @@
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+
+
+data = pd.read_csv("Datasets/Salary_Data.csv")
+X = data.iloc[ : , :-1 ].values
+Y = data.iloc[ : , -1 ].values
+
+
+
+print(X)
+print(Y)
+
